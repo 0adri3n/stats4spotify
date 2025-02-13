0e7ff7ad2106f2aff4c0e28736e899bc8bceb395
0adri3n <0adri3n@users.noreply.github.com>
2025-02-13 22:26:38 +0100

    edit js

--- src/js/script.js
+++ src/js/script.js
@@ -141,6 +141,8 @@ if (location.search.indexOf("#access_token") > 0) {
   Auth.parseResponse(location);
   window.location = SITE_URL;
 } else {
+  console.log(location.search.indexOf("#access_token"));
+  console.log(Config)
   if (Config.getValidToken()) {
     SpotifyApi.getUserProfile();
     SpotifyApi.getTopTracks();
