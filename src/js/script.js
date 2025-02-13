7825c247d0e2f2ef59bc23103503626911c886b4
0adri3n <0adri3n@users.noreply.github.com>
2025-02-13 22:14:18 +0100

    working. keep editing

--- src/js/script.js
+++ src/js/script.js
@@ -1,368 +1,148 @@
-function onGoogleLoaded() {
-  var SITE_URL = "https://0adri3n.github.io/stats4spotify/";
+var SITE_URL = "https://0adri3n.github.io/stats4spotify";
+var SPOTIFY_API = "https://api.spotify.com/v1";
 
-  var SPOTIFY_API = "https://api.spotify.com/v1";
-
-  var HIGHLIGHT_TIMEOUT = 100;
-
-
-
-  function parseUri(uri, delimiter) {
-    var segments = uri.split(delimiter || ":");
-    var id, type;
-    if (segments[1] === "user") {
-      type = segments[3];
-      id = segments[2] + ":" + segments[4];
-    } else if (segments[1] === "album") {
-      type = segments[1];
-      id = segments[2];
+var SpotifyApi = {
+  sendRequest: function (url, callback) {
+    var accessToken = Config.getValidToken();
+    if (accessToken == null) {
+      console.error("Unauthorized: No valid token");
+      callback("unauthorised");
+      return;
     }
-    return { type: type, id: id };
-  }
+    d3.json(url)
+      .header("Authorization", "Bearer " + accessToken)
+      .get(callback);
+  },
 
-  function validateAndPerformSearch(parsedUri) {
-    if (parsedUri.type == "album") {
-      document.body.classList.remove("invalid-search");
-      performSearch(parsedUri.id);
-    } else {
-      document.body.classList.add("invalid-search");
-      clearChart();
-    }
-  }
+  getUserProfile: function () {
+    this.sendRequest(SPOTIFY_API + "/me", function (error, data) {
+      if (error) {
+        console.error("Error fetching profile:", error);
+        return;
+      }
+      console.log("User Profile:", data);
+      document.querySelector(".profile img").src =
+        data.images[0]?.url || "default.png";
+      document.querySelector(".profile h2").textContent = data.display_name;
+      document.querySelector(
+        ".profile p"
+      ).textContent = `Followers: ${data.followers.total}`;
+    });
+  },
 
-
-  function performSearch(id) {
-    Config.setLastSearch(id);
-    infoContainer.innerHTML = "";
-
-    SpotifyApi.sendRequest(
-      SPOTIFY_API + "/albums/" + id,
-      function (error, album) {
-        if (error && error == "unauthorised") {
-          document.body.classList.add("no-token");
-          clearChart();
+  getTopTracks: function () {
+    this.sendRequest(
+      SPOTIFY_API + "/me/top/tracks?limit=5",
+      function (error, data) {
+        if (error) {
+          console.error("Error fetching top tracks:", error);
           return;
         }
-
-        fillInfo(album);
-
-        function albumsCallback(result, next) {
-          if (next) {
-            getAlbums(next, album.name, albumsCallback, result);
-          } else {
-            fillInfoData(result, album);
-            fillChartData(result);
-          }
-        }
-
-        var uri =
-          SPOTIFY_API +
-          "/artists/" +
-          album.artists[0].id +
-          "/albums?album_type=" +
-          album.album_type;
-
-        var markets = [];
-        var ids = [];
-        var count = {};
-        if (album.available_markets.length) {
-          markets.push(album.available_markets);
-          ids.push(album.id);
-          album.available_markets.forEach(function (market) {
-            count[market] = 1;
-          });
-        }
-
-        getAlbums(uri, album.name, albumsCallback, {
-          markets: markets,
-          ids: ids,
-          count: count,
+        console.log("Top Tracks:", data.items);
+        var container = document.querySelector(".top-items.tracks");
+        container.innerHTML = "";
+        data.items.forEach((track) => {
+          container.innerHTML += `
+          <div class="top-item">
+            <img src="${track.album.images[0]?.url}" alt="Track">
+            <p>${track.name}</p>
+          </div>
+        `;
         });
       }
     );
-  }
+  },
 
-  function getAlbums(uri, albumName, callback, result) {
-    SpotifyApi.sendRequest(uri, function (error, albums) {
-      albums.items.forEach(function (el) {
-        var elName = el.name.toLowerCase().replace(/[,.;:?!"]/g, "");
-        var alName = albumName.toLowerCase().replace(/[,.;:?!"]/g, "");
-        var condition = Config.getExactSearch()
-          ? el.name == alName
-          : elName.indexOf(alName) == 0 || alName.indexOf(elName) == 0;
-        if (condition && result.ids.indexOf(el.id) == -1) {
-          result.ids.push(el.id);
-          result.markets.push(el.available_markets || []);
-          if (el.available_markets) {
-            el.available_markets.forEach(function (market) {
-              result.count[market] = result.count[market] || 0;
-              result.count[market]++;
-            });
-          }
+  getTopArtists: function () {
+    this.sendRequest(
+      SPOTIFY_API + "/me/top/artists?limit=5",
+      function (error, data) {
+        if (error) {
+          console.error("Error fetching top artists:", error);
+          return;
         }
-      });
+        console.log("Top Artists:", data.items);
+        var container = document.querySelector(".top-items.artists");
+        container.innerHTML = "";
+        data.items.forEach((artist) => {
+          container.innerHTML += `
+          <div class="top-item">
+            <img src="${artist.images[0]?.url}" alt="Artist">
+            <p>${artist.name}</p>
+          </div>
+        `;
+        });
+      }
+    );
+  },
+};
 
-      callback(result, albums.next);
-    });
-  }
+var Auth = {
+  getAuthUrl: function (siteUrl) {
+    var clientId = "9e5bc3ca56f746b49e1e1f7049706043";
+    var redirectUri = encodeURIComponent(siteUrl + "/index.html");
+    return `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=user-read-private user-read-email user-top-read`;
+  },
 
-  function fillInfo(info) {
-    var data = {
-      albumTitle: info.name,
-      albumType: info.album_type,
-      artistName: info.artists
-        .map(function (el) {
-          return el.name;
-        })
-        .join(", "),
-    };
-    var rendered = Mustache.render(albumInfoBigTemplate, data);
-    albumInfoBig.innerHTML = rendered;
-  }
-
-  var currentDataset;
-
-  function highlight(id) {
-    if (currentDataset) {
-      var markets = [];
-      var dataset = currentDataset.map(function (el) {
-        var row = el.slice(0);
-        row.spid = el.spid;
-        if (row.spid && row.spid == id) {
-          markets.push(row[0]);
-          row[1] = 0.5;
-        }
-        return row;
-      });
-      drawChart(
-        dataset.filter(function (el) {
-          return markets.indexOf(el[0]) == -1 || el.spid == id;
-        })
-      );
+  parseResponse: function (url) {
+    var hash = url.hash;
+    var response = this._parseHash(hash);
+    if (response) {
+      Config.setToken(response["access_token"]);
+      Config.setExpiresAt(response["expires_in"]);
+      SpotifyApi.getUserProfile();
+      SpotifyApi.getTopTracks();
+      SpotifyApi.getTopArtists();
     }
-  }
+  },
 
-  function getAlbumInfo(name, albums, id) {
-    var fragment = document.createDocumentFragment();
-    var title = document.createElement("h3");
-    title.classList.add("row");
-    title.classList.add("album-title");
-    title.innerHTML = name;
+  _parseHash: function (hash) {
+    if (!hash) return null;
+    return hash
+      .replace("#", "")
+      .split("&")
+      .reduce((acc, el) => {
+        let [key, value] = el.split("=");
+        acc[key] = value;
+        return acc;
+      }, {});
+  },
+};
 
-    fragment.appendChild(title);
+var Config = {
+  getValidToken: function () {
+    var expiresAt = parseInt(localStorage.getItem("token_expires_at"), 10);
+    var token = localStorage.getItem("token");
+    var now = Date.now();
+    return token && expiresAt > now ? token : null;
+  },
 
-    albums.forEach(function (album) {
-      var div = document.createElement("div");
-      div.dataset.albumid = album.id;
-      div.classList.add("album-info");
-      div.classList.add("row");
+  setToken: function (accessToken) {
+    localStorage.setItem("token", accessToken);
+  },
 
-      if (album.id == id) {
-        div.classList.add("searched");
-      }
+  setExpiresAt: function (expiresIn) {
+    var now = Date.now();
+    localStorage.setItem(
+      "token_expires_at",
+      now + parseInt(expiresIn, 10) * 1000
+    );
+  },
+};
 
-      var explicit = false;
-      for (var i = 0; i < album.tracks.items.length; i++) {
-        if (album.tracks.items[i].explicit) {
-          explicit = true;
-          break;
-        }
-      }
-
-      var copyrights = d3.map(album.copyrights, function (d) {
-        return d.type;
-      });
-
-      var html = "";
-      html += '<a href="https://open.spotify.com/album/' + album.id + '">';
-      html += '<img class="cover" src="' + album.images[2].url + '" alt="" />';
-      html += "</a>";
-      html += "<ul>";
-      html += '<li><a href="https://open.spotify.com/album/' + album.id + '">';
-      html +=
-        album.tracks.items.length +
-        " tracks on " +
-        album.tracks.items.pop().disc_number +
-        " discs";
-      html += "</a></li>";
-      html += "<li>" + album.album_type + " &bull; ";
-      html += album.release_date + "</li>";
-      if (explicit) {
-        html += '<li><span class="explicit">Explicit</span>&nbsp;</li>';
-      }
-      html += "</ul>";
-      html +=
-        "<p>" +
-        album.available_markets
-          .map(function (el) {
-            return '<span title="' + ALL_MARKETS[el] + '">' + el + "</span>";
-          })
-          .join(" ") +
-        "</p>";
-      html +=
-        '<p class="uri"><a href="https://api.spotify.com/v1/albums/' +
-        album.id +
-        '">' +
-        album.uri +
-        "</a></p>";
-      if (copyrights.get("C")) {
-        html += '<p class="copyrights">&copy; ' + copyrights.get("C").text;
-        +"</p>";
-      }
-      if (copyrights.get("P")) {
-        html +=
-          '<p class="copyrights">&#8471; ' + copyrights.get("P").text + "</p>";
-      }
-      div.innerHTML = html;
-
-      fragment.appendChild(div);
-    });
-    return fragment;
-  }
-
-  function fillInfoData(result, album) {
-    var ids = result.ids;
-    if (ids.length) {
-      SpotifyApi.sendRequest(
-        SPOTIFY_API + "/albums?ids=" + ids,
-        function (error, albums) {
-          console.log("Full found albums", albums.albums);
-          var nest = d3
-            .nest()
-            .key(function (d) {
-              return d.name;
-            })
-            .sortKeys(function (a, b) {
-              return a == album.name ? 1 : -1;
-            })
-            .sortValues(function (a, b) {
-              return a.id == album.id ? -1 : 1;
-            })
-            .entries(albums.albums);
-
-          nest.forEach(function (el) {
-            infoContainer.insertBefore(
-              getAlbumInfo(el.key, el.values, album.id),
-              infoContainer.firstChild
-            );
-          });
-
-          if (!infoContainer.querySelector(".searched")) {
-            exactMatchNotFound(album);
-          }
-        }
-      );
-    } else {
-      exactMatchNotFound(album);
-    }
-  }
-
-  function exactMatchNotFound(album) {
-    var html = Mustache.render(albumNotFoundTemplate, {
-      albumApiLink: album.href,
-      albumUri: album.uri,
-    });
-    infoContainer.innerHTML = html + infoContainer.innerHTML;
-  }
-
-  function fillChartData(result) {
-    var marketsList = result.markets;
-    var ids = result.ids;
-    var data = [];
-    var allMarkets = Object.keys(ALL_MARKETS);
-
-    marketsList.forEach(function (markets, i) {
-      markets.forEach(function (market) {
-        var index = allMarkets.indexOf(market);
-        if (index > -1) {
-          allMarkets.splice(allMarkets.indexOf(market), 1);
-        }
-
-        var name = market;
-        if (ALL_MARKETS[market]) {
-          name = ALL_MARKETS[market];
-        } else {
-          console.log("Not in available markets", market);
-        }
-
-        var el = [
-          name,
-          result.count[market],
-          result.count[market] + " versions available",
-        ];
-        el.spid = ids[i];
-        data.push(el);
-      });
-    });
-
-    if (allMarkets.length) {
-      console.log("Not available in: " + allMarkets);
-      var html = Mustache.render(albumNotAvailableTemplate, {
-        markets: allMarkets.map(function (el) {
-          return { code: el, name: ALL_MARKETS[el] };
-        }),
-      });
-      infoContainer.innerHTML += html;
-    }
-
-    allMarkets.forEach(function (market) {
-      data.push([ALL_MARKETS[market], 0, "Not available"]);
-    });
-
-    console.log("Chart data", data);
-    currentDataset = data;
-    drawChart(data);
-  }
-
-  function drawChart(markets) {
-    var chartData = new google.visualization.DataTable();
-    chartData.addColumn("string", "Country");
-    chartData.addColumn("number", "Available");
-    chartData.addColumn({ type: "string", role: "tooltip" });
-    chartData.addRows(markets);
-
-    var max =
-      d3.max(markets, function (el) {
-        return el[1];
-      }) || 1;
-
-    if (max < 1) max = 1;
-
-    var options = {
-      backgroundColor: "transparent",
-      datalessRegionColor: "#2e2f33",
-      keepAspectRatio: true,
-      colorAxis: {
-        minValue: 0,
-        colors: ["#8f2600", "#bdaa00", "#4B6601", "#B4D612"],
-        values: [0, 0.5, 1, max],
-      },
-      legend: "none",
-      tooltip: {
-        showColorCode: false,
-      },
-      enableRegionInteractivity: true,
-      animation: {
-        duration: 1000,
-        easing: "out",
-      },
-    };
-
-    chart.draw(chartData, options);
-  }
-
-  function authenticate() {
-    var authUrl = Auth.getAuthUrl(SITE_URL);
-    window.location = authUrl;
-  }
-
-  if (location.search.indexOf("auth_callback") > 0) {
-    Auth.parseResponse(location);
-    window.location = SITE_URL;
-    return;
-  }
-
-  chart = new google.visualization.GeoChart(chartContainer);
-
-  performSearch(Config.getLastSearch() || "18qY7zpuNqeXNGywRysjxx");
+function authenticate() {
+  window.location = Auth.getAuthUrl(SITE_URL);
 }
+
+if (location.search.indexOf("auth_callback") > 0) {
+  Auth.parseResponse(location);
+  window.location = SITE_URL;
+} else {
+  if (Config.getValidToken()) {
+    SpotifyApi.getUserProfile();
+    SpotifyApi.getTopTracks();
+    SpotifyApi.getTopArtists();
+  }
+}
+
+document.querySelector("#login-button").addEventListener("click", authenticate);
